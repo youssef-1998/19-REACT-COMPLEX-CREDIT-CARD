b4d4e165cea5de882feb7b5e9fb57a51943549c4
youssef-1998 <bouzar26@gmail.com>
2019-11-19 18:51:41 +0100

    Updates

---
+++ precache-manifest.50c55139fd13d146ef5a58b37f351b63.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "901321f0e72f6d9505d8e0ed3e009090",
+    "url": "/REACT-COMPLEX-CREDIT-CARD/index.html"
+  },
+  {
+    "revision": "26c14f0a90c456f8497d",
+    "url": "/REACT-COMPLEX-CREDIT-CARD/static/css/main.b632a9b7.chunk.css"
+  },
+  {
+    "revision": "ec9cda7c1a873bbfe39c",
+    "url": "/REACT-COMPLEX-CREDIT-CARD/static/js/2.594cad36.chunk.js"
+  },
+  {
+    "revision": "26c14f0a90c456f8497d",
+    "url": "/REACT-COMPLEX-CREDIT-CARD/static/js/main.67c08d03.chunk.js"
+  },
+  {
+    "revision": "2bd16f8ec17a3f51c4b3",
+    "url": "/REACT-COMPLEX-CREDIT-CARD/static/js/runtime-main.32568b1b.js"
+  },
+  {
+    "revision": "d974f18a9c02d633b0e56cc434071b79",
+    "url": "/REACT-COMPLEX-CREDIT-CARD/static/media/BungeeHairline-Regular.d974f18a.ttf"
+  },
+  {
+    "revision": "1aff84911778a4de8c9ac27f1aab2396",
+    "url": "/REACT-COMPLEX-CREDIT-CARD/static/media/background.1aff8491.jpg"
+  },
+  {
+    "revision": "7cf0ee8b7a9e8287267792040650dc7a",
+    "url": "/REACT-COMPLEX-CREDIT-CARD/static/media/puce 1.7cf0ee8b.png"
+  },
+  {
+    "revision": "260255dd28304881a30a1717edd973ad",
+    "url": "/REACT-COMPLEX-CREDIT-CARD/static/media/visa.260255dd.jpeg"
+  }
+]);
